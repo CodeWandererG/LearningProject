dfe5eeafee0a5b7b6435f7bd29fd94b469010351
GurjeetSingh7000 <101127973+GurjeetSingh7000@users.noreply.github.com>
2024-04-12 00:54:01 +0530

    doing the name editable

---
+++ components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({initialName , symbol}) {
+    const [playerName , setPlayerName] = useState(initialName); 
+    const [edit ,setEdit] = useState(false);
+    const editPlayer = () => {
+        setEdit(wasEdit => !wasEdit);
+        // setEdit((wasEdit) => !wasEdit);  set value instantly 
+    }
+
+    const handleChange = (event) => {
+        console.log(event.target.value);
+        setPlayerName(event.target.value);
+    }
+
+    let editPlayerName = <span className="player-name">{playerName}</span>
+    if(edit){
+        // case of two way binding -> 1. getting the playerName value , 2. Changing the value
+        editPlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+    }
+    return (
+        <>
+        <li>
+            <span className="player">
+            { editPlayerName }
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={editPlayer}>{edit ? "Save" : "Edit"}</button>
+          </li>
+        </>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Player from "../components/Player"
 
 function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <>
+      <div className="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X"/>          
+          <Player initialName="Player 2" symbol="0"/> 
+        </ol>
+      </div>
+      <h1>Game Board</h1>
+    </>
   )
 }
 
-export default App
+export default App;
